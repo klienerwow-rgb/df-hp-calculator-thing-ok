c07cd7a60398be9ff5de125e4325a6c163dc0cfa
whatwar <mikaylahansen.p@gmail.com>
2026-09-24 17:56:03 -0400

    g

--- script.js
+++ script.js
@@ -4,8 +4,38 @@ function calculate() {
     var dfp = Number(document.getElementById("dfp").value)
     var damage = Number(document.getElementById("damage").value)
 
+    var damageBeforePercent = damage + df
     var multiplier = 1 - dfp / 100
-    var taken = (damage - df) * multiplier
+    var taken = damageBeforePercent * multiplier
+
+    var hits = Infinity
+    var ehp = Infinity
+
+    if (taken > 0 && damage > 0 && hp > 0) {
+        hits = Math.ceil(hp / taken)
+        ehp = hp / (taken / damage)
+    }
+
+    document.getElementById("output").innerHTML =
+        "Damage Taken: " + format(taken) + "<br>" +
+        "Damage Multiplier: " + format(multiplier) + "x<br>" +
+        "Hits To Kill: " + format(hits) + "<br>" +
+        "Effective HP: " + format(ehp)
+}
+
+function format(value) {
+    if (!Number.isFinite(value)) {
+        return "∞"
+    }
+function calculate() {
+    var hp = Number(document.getElementById("hp").value)
+    var df = Number(document.getElementById("df").value)
+    var dfp = Number(document.getElementById("dfp").value)
+    var damage = Number(document.getElementById("damage").value)
+
+    var damageBeforePercent = damage + df
+    var multiplier = 1 - dfp / 100
+    var taken = damageBeforePercent * multiplier
 
     var hits = Infinity
     var ehp = Infinity
